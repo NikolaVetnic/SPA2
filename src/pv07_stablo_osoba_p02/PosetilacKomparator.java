f8b63617fc8b2fca1145ba05f1ae7f57359810f7
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2021-01-10 21:38:49 +0100

    Vezba zadataka iz stabala.

---
+++ src/pv07_stablo_osoba_p02/PosetilacKomparator.java
@@ -0,0 +1,26 @@
+package pv07_stablo_osoba_p02;
+
+import java.util.Comparator;
+
+import pv07_stablo_osoba_p02.StabloOsoba.Cvor;
+
+public class PosetilacKomparator implements IPoseti {
+	
+	Comparator<Osoba> c;
+	Osoba o;
+	
+	public PosetilacKomparator(Comparator<Osoba> c) {
+		this.c = c;
+		this.o = null;
+	}
+
+	@Override
+	public void posetiCvor(Cvor cvor) {
+		
+		if (this.o == null)
+			this.o = cvor.osoba;
+		else 
+			if (c.compare(cvor.osoba, o) >= 0)
+				this.o = cvor.osoba;
+	}
+}

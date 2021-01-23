f0a29751b1e2225f619262b1f5e86fc96904fc28
user <user@WINDOWS-ONKAL5M>
2021-01-23 13:31:42 +0100

    Minimalno rastojanje tacaka, pretraga stringova - usmeni.

---
+++ src/p_vezba/qB16_string_distance/Point2D.java
@@ -0,0 +1,25 @@
+package p_vezba.qB16_string_distance;
+
+public class Point2D {
+
+	public double x, y;
+	
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public boolean coincident(Point2D p) {
+		return x == p.x && y == p.y;
+	}
+	
+	public double dist(Point2D p) {
+		double x2 = (x - p.x) * (x - p.x);
+		double y2 = (y - p.y) * (y - p.y);
+		return Math.sqrt(x2 + y2);
+	}
+	
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+}
---
+++ src/p_vezba/qB17_bf_rk_search/BruteForceSearch.java
@@ -0,0 +1,26 @@
+package p_vezba.qB17_bf_rk_search;
+
+public class BruteForceSearch {
+
+	public static int search(String pat, String txt) {
+		
+		if (pat.length() > txt.length())
+			throw new IllegalArgumentException("Pattern duzi od teksta");
+		
+		for (int i = 0; i < txt.length() - pat.length(); i++) {
+			
+			boolean match = true;
+			int j = 0;
+			
+			while (match && j < pat.length())
+				if (pat.charAt(j) != txt.charAt(i + j))
+					match = false;
+				else
+					j++;
+			
+			if (match) return i;
+		}
+		
+		return -1;
+	}
+}
